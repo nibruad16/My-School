ae1bc4789d1c1b9b08ab86c6c02f0738f73b14fe
nibruad16 <nibruad16@gmail.com>
2025-08-26 11:07:19 +0300

    dashbord page

--- lib/presentation/pages/dashboard_page.dart
+++ lib/presentation/pages/dashboard_page.dart
@@ -1,1 +1,34 @@
+// lib/features/auth/presentation/pages/dashboard_page.dart
+import 'package:flutter/material.dart';
 
+class DashboardPage extends StatelessWidget {
+  const DashboardPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('My School Dashboard'),
+        backgroundColor: const Color(0xFF26A69A), // Consistent green color
+        foregroundColor: Colors.white,
+      ),
+      body: const Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Welcome to My School!',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 10),
+            Text(
+              'Your journey to efficient school management begins here.',
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 16, color: Colors.grey),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
